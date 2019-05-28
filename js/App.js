9aa1eb7bd5c962a02931095ddadd6e6c34a856bb
Travis CI User <travis@example.org>
2019-05-28 12:07:22 +0000

    Updates deps (#329)

--- js/App.js
+++ js/App.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e980bf791300ae672579";
+/******/ 	var hotCurrentHash = "603b51bb051f1fa094d9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
