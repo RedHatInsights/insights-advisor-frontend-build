409758fded033c4ce0b1ee9f423cf37fce83a7db
Travis CI User <travis@example.org>
2020-09-24 12:00:00 +0000

    Mark systems table as full view (#758)

---
+++ js/SystemsTable~TopicAdmin.bab67a6a95488a642c54.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{1025:function(e,a,n){"use strict";var r=n(483),t=n(484),s=n(486),l=n(905),c=n.n(l),m=n(1),p=n.n(m),u=n(10),d=n.n(u),i=function(e){var a=e.message;return p.a.createElement(p.a.Fragment,null,p.a.createElement(r.Card,{className:"ins-empty-rule-cards"},p.a.createElement(s.CardHeader,null,p.a.createElement(c.a,{size:"lg"})),p.a.createElement(t.CardBody,null,a)))};a.a=i,i.propTypes={message:d.a.string}}}]);
+//# sourceMappingURL=SystemsTable~TopicAdmin.bab67a6a95488a642c54.js.map
