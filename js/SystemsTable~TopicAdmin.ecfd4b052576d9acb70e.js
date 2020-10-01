30aed28d25105c1d10cc7f4c14a8d4280ccf1b8e
Travis CI User <travis@example.org>
2020-10-01 11:59:13 +0000

    Workload query building improvement, less code

--- js/SystemsTable~TopicAdmin.ecfd4b052576d9acb70e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{1165:function(e,a,n){"use strict";var r=n(547),t=n(548),s=n(551),l=n(1035),c=n.n(l),m=n(1),p=n.n(m),u=n(12),d=n.n(u),i=function(e){var a=e.message;return p.a.createElement(p.a.Fragment,null,p.a.createElement(r.Card,{className:"ins-empty-rule-cards"},p.a.createElement(s.CardHeader,null,p.a.createElement(c.a,{size:"lg"})),p.a.createElement(t.CardBody,null,a)))};a.a=i,i.propTypes={message:d.a.string}}}]);
-//# sourceMappingURL=SystemsTable~TopicAdmin.ecfd4b052576d9acb70e.js.map
