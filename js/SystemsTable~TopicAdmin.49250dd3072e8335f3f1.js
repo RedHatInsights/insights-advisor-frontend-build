e50c00aa24be71024a91ece2e183517d397cb54b
Travis CI User <travis@example.org>
2020-10-22 13:07:05 +0000

    Change rule_status param to suit api change (#775)
    fixes https://issues.redhat.com/browse/ADVISOR-1419

---
+++ js/SystemsTable~TopicAdmin.49250dd3072e8335f3f1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1182:function(e,a,n){"use strict";var r=n(574),t=n(575),s=n(581),l=n(1063),c=n.n(l),m=n(1),p=n.n(m),u=n(12),d=n.n(u),i=function(e){var a=e.message;return p.a.createElement(p.a.Fragment,null,p.a.createElement(r.Card,{className:"ins-empty-rule-cards"},p.a.createElement(s.CardHeader,null,p.a.createElement(c.a,{size:"lg"})),p.a.createElement(t.CardBody,null,a)))};a.a=i,i.propTypes={message:d.a.string}}}]);
+//# sourceMappingURL=SystemsTable~TopicAdmin.49250dd3072e8335f3f1.js.map
