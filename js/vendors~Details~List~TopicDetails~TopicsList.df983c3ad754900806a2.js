a175e0da33b2cdbc1b135b2b1c17b3cfdd9d5baf
Travis CI User <travis@example.org>
2020-10-26 16:14:42 +0000

    Removes tagstoolbar now that global filter exists🙇‍♂️📚 (#776)
    fixes https://issues.redhat.com/browse/ADVISOR-1434

---
+++ js/vendors~Details~List~TopicDetails~TopicsList.df983c3ad754900806a2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1127:function(n,o){n.exports=function(n,o){for(var r=-1,t=null==n?0:n.length,u=Array(t);++r<t;)u[r]=o(n[r],r,n);return u}},1158:function(n,o){n.exports=function(n){return function(o){return n(o)}}}}]);
+//# sourceMappingURL=vendors~Details~List~TopicDetails~TopicsList.df983c3ad754900806a2.js.map
--- js/vendors~TopicsList.6255e4781da0eda0a732.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{1900:function(n,o,r){var e=r(1589),p=r(1577),t=r(580),u=r(588),w=t((function(n,o){if(null==n)return[];var r=o.length;return r>1&&u(n,o[0],o[1])?o=[]:r>2&&u(o[0],o[1],o[2])&&(o=[o[0]]),p(n,e(o,1),[])}));n.exports=w}}]);
-//# sourceMappingURL=vendors~TopicsList.6255e4781da0eda0a732.js.map
