46d0400220cbd305ae10691d7ae710292d572261
Travis CI User <travis@example.org>
2020-10-27 18:52:39 +0000

    Display impacted_systems_count for disabled recommendations too (#774)

---
+++ js/SystemsTable~TopicAdmin.5519bc55dbd83389fff6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1131:function(e,a,n){"use strict";var r=n(564),t=n(565),s=n(572),l=n(1041),c=n.n(l),m=n(1),p=n.n(m),u=n(12),d=n.n(u),i=function(e){var a=e.message;return p.a.createElement(p.a.Fragment,null,p.a.createElement(r.Card,{className:"ins-empty-rule-cards"},p.a.createElement(s.CardHeader,null,p.a.createElement(c.a,{size:"lg"})),p.a.createElement(t.CardBody,null,a)))};a.a=i,i.propTypes={message:d.a.string}}}]);
+//# sourceMappingURL=SystemsTable~TopicAdmin.5519bc55dbd83389fff6.js.map
