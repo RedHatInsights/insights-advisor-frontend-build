9e277936365d25e718185df2bdd64efcc32ef8aa
Travis CI User <travis@example.org>
2020-11-10 20:20:30 +0000

    Adds RBAC to view advisor, export data, disable rules (#781)
    fixes https://issues.redhat.com/browse/ADVISOR-1092

--- js/vendors~Details~List~TopicDetails~TopicsList.35db1f3960efce4395fb.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1142:function(n,o){n.exports=function(n,o){for(var r=-1,t=null==n?0:n.length,u=Array(t);++r<t;)u[r]=o(n[r],r,n);return u}},1161:function(n,o){n.exports=function(n){return function(o){return n(o)}}}}]);
-//# sourceMappingURL=vendors~Details~List~TopicDetails~TopicsList.35db1f3960efce4395fb.js.map
