635710d77908045c5ea02e975b781fa67ea3370d
Travis CI User <travis@example.org>
2020-11-20 21:29:52 +0000

    Correct rbac perm naming

---
+++ js/vendors~Details~List~TopicDetails~TopicsList.f03a62372636ddc4d826.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1174:function(n,o){n.exports=function(n,o){for(var r=-1,t=null==n?0:n.length,u=Array(t);++r<t;)u[r]=o(n[r],r,n);return u}},1194:function(n,o){n.exports=function(n){return function(o){return n(o)}}}}]);
+//# sourceMappingURL=vendors~Details~List~TopicDetails~TopicsList.f03a62372636ddc4d826.js.map
