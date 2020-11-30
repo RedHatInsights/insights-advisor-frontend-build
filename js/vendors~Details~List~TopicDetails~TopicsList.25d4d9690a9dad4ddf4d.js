434f5a5752692f6fa430625d0f91cf95cd38867d
Travis CI User <travis@example.org>
2020-11-30 21:14:16 +0000

    Store the alert close action in local storage (.. and update deps) (#785)
    Fixes https://issues.redhat.com/browse/ADVISOR-1496
    
    Co-authored-by: Allen Wight <awight@redhat.com>

---
+++ js/vendors~Details~List~TopicDetails~TopicsList.25d4d9690a9dad4ddf4d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1174:function(n,o){n.exports=function(n,o){for(var r=-1,t=null==n?0:n.length,u=Array(t);++r<t;)u[r]=o(n[r],r,n);return u}},1194:function(n,o){n.exports=function(n){return function(o){return n(o)}}}}]);
+//# sourceMappingURL=vendors~Details~List~TopicDetails~TopicsList.25d4d9690a9dad4ddf4d.js.map
