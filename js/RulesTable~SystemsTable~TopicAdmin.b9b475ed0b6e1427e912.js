0bfc2021827cf7578d6576f72f3fa56647b2e0b2
Travis CI User <travis@example.org>
2021-01-04 19:13:03 +0000

    Rollback inv-insights, not yet readyf

--- js/RulesTable~SystemsTable~TopicAdmin.b9b475ed0b6e1427e912.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{1205:function(e,a,n){"use strict";var r=n(342),t=n(614),s=n(632),l=n(1117),c=n.n(l),m=n(1),p=n.n(m),u=n(10),d=n.n(u),i=function(e){var a=e.message;return p.a.createElement(p.a.Fragment,null,p.a.createElement(r.Card,{className:"ins-empty-rule-cards"},p.a.createElement(s.CardHeader,null,p.a.createElement(c.a,{size:"lg"})),p.a.createElement(t.CardBody,null,a)))};a.a=i,i.propTypes={message:d.a.string}}}]);
-//# sourceMappingURL=RulesTable~SystemsTable~TopicAdmin.b9b475ed0b6e1427e912.js.map
