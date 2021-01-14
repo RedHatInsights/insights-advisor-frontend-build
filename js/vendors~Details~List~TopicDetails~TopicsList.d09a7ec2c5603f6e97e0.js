c13e7e26ee4688be6514d2f5c0ab8fc6a03fc2b9
Travis CI User <travis@example.org>
2021-01-14 13:31:38 +0000

    Added ouiaid attributes to all UI components (#791)

--- js/vendors~Details~List~TopicDetails~TopicsList.d09a7ec2c5603f6e97e0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1189:function(n,o){n.exports=function(n,o){for(var r=-1,t=null==n?0:n.length,u=Array(t);++r<t;)u[r]=o(n[r],r,n);return u}},1209:function(n,o){n.exports=function(n){return function(o){return n(o)}}}}]);
-//# sourceMappingURL=vendors~Details~List~TopicDetails~TopicsList.d09a7ec2c5603f6e97e0.js.map
