0ad3bc929fc07e60c6cf3aeb0df175f6686aa59a
Travis CI User <travis@example.org>
2021-01-29 12:30:32 +0000

    Update deps, fec v2 👉 v3 🥰🥰🥰🥰🥰 (#793)
    https://github.com/RedHatInsights/frontend-components/blob/master/packages/components/README.md#migration-v2---v3 IS THE WAY

---
+++ js/9900.4f7a5292adff5d481f99.js
@@ -0,0 +1,2 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[9900],{59900:(e,n,r)=>{"use strict";r.d(n,{Z:()=>o});var a=r(2095),t=r(85962),s=r(21015),l=r(62965),c=r(97522),d=r.n(c),m=r(45697),u=function(e){var n=e.message;return d().createElement(d().Fragment,null,d().createElement(a.Card,{className:"ins-empty-rule-cards"},d().createElement(s.CardHeader,null,d().createElement(l.ZP,{size:"lg"})),d().createElement(t.CardBody,null,n)))};const o=u;u.propTypes={message:r.n(m)().string}}}]);
+//# sourceMappingURL=9900.4f7a5292adff5d481f99.js.map
