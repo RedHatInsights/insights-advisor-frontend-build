015e10ab15024c218efa274b9419e546724fcdbb
Travis CI User <travis@example.org>
2021-03-03 20:03:10 +0000

    Hotfix - Cease rhdisabled api calls once the notification is dismissed

--- js/2489.71065bf612920e4b25bb.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[2489],{52489:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=2489.71065bf612920e4b25bb.js.map
