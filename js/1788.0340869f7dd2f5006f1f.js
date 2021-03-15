8608453fce8a17d17b4fda523fad3d5686e53d62
Travis CI User <travis@example.org>
2021-03-15 19:06:31 +0000

    Removes local-proxy requirement, update deps (#803)
    fixes https://issues.redhat.com/browse/ADVISOR-1624

--- js/1788.0340869f7dd2f5006f1f.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[1788],{41788:(t,e,o)=>{"use strict";o.d(e,{Z:()=>n});var r=o(14665);function n(t,e){t.prototype=Object.create(e.prototype),t.prototype.constructor=t,(0,r.Z)(t,e)}}}]);
-//# sourceMappingURL=1788.0340869f7dd2f5006f1f.js.map
