f0e0bb14dcc1336b0d496b1c6a73f9357075d757
Travis CI User <travis@example.org>
2021-04-19 11:06:05 +0000

    Adds search to rec details system table, refactors details page (#811)

---
+++ js/4330.469d534e31c26540359b.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[4330],{24330:(e,n,t)=>{"use strict";t.d(n,{Z:()=>o});var u=t(28481),r=t(93264);const o=function(e,n){var t=(0,r.useState)(e),o=(0,u.Z)(t,2),s=o[0],c=o[1];return(0,r.useEffect)((function(){var t=setTimeout((function(){c(e)}),n);return function(){clearTimeout(t)}}),[n,e]),s}}}]);
