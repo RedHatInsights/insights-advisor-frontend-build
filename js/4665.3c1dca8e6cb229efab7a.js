67cd38da1d1721c07debcb6028dc7e741aaf0d99
Travis CI User <travis@example.org>
2021-04-24 00:03:57 +0000

    Tinkering with rec details system table to reduce api calls 🧐

--- js/4665.3c1dca8e6cb229efab7a.js
+++ js/4665.3c1dca8e6cb229efab7a.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[4665],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/4665.dab8d1d0d87c0445c493.js.map
+//# sourceMappingURL=../sourcemaps/4665.41e641e16a453d37634e.js.map
