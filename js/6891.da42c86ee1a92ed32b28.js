ec781f07995e504289d96d21339386f33144e5e2
Travis CI User <travis@example.org>
2021-04-24 00:41:54 +0000

    Fix breadcrumb white space for new chrome layout on rec details

--- js/6891.da42c86ee1a92ed32b28.js
+++ js/6891.da42c86ee1a92ed32b28.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/6891.a8d1f5459ef071d71e47.js.map
+//# sourceMappingURL=../sourcemaps/6891.d7c4ed207eb296b14369.js.map
