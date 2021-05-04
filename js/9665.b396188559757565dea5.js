2c296012d72c9fe7f44ed012fd4dfe07324bf341
Travis CI User <travis@example.org>
2021-05-04 14:00:52 +0000

    Disable default columns in inventory table (#820)

--- js/9665.b396188559757565dea5.js
+++ js/9665.b396188559757565dea5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[9665],{28368:(o,n,r)=>{var e=r(90939);o.exports=function(o,n,r){var i=(r="function"==typeof r?r:void 0)?r(o,n):void 0;return void 0===i?e(o,n,void 0,r):!!i}},30236:(o,n,r)=>{var e=r(42980),i=r(21463)((function(o,n,r,i){e(o,n,r,i)}));o.exports=i}}]);
-//# sourceMappingURL=../sourcemaps/9665.4a10f0adeacb2dc059fd.js.map
+//# sourceMappingURL=../sourcemaps/9665.95cff0211abbae5891b6.js.map
