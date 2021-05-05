b816c932965282fcbc60a9c86a6b9a77f71785f1
Travis CI User <travis@example.org>
2021-05-05 13:25:19 +0000

    Bugfix:SystemAdvisor category filter is working correctly 🥸

--- js/1788.e49f552760133ceacde0.js
+++ js/1788.e49f552760133ceacde0.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[1788],{41788:(t,e,o)=>{"use strict";o.d(e,{Z:()=>n});var r=o(14665);function n(t,e){t.prototype=Object.create(e.prototype),t.prototype.constructor=t,(0,r.Z)(t,e)}}}]);
-//# sourceMappingURL=../sourcemaps/1788.958cbacdb851138e60bd.js.map
+//# sourceMappingURL=../sourcemaps/1788.bd15b043f5e874d4e550.js.map
