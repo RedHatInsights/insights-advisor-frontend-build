f3e146d2616b032c66f6cfa869556fc1cf3591a6
Travis CI User <travis@example.org>
2021-05-05 12:32:58 +0000

    Move/update system detail from FEC to advisor (#818)
    Co-authored-by: Allen Wight <awight@redhat.com>

--- js/609.18fa7d17f702b4ea8479.js
+++ js/609.18fa7d17f702b4ea8479.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[609],{40609:(e,n,t)=>{"use strict";t.d(n,{Z:()=>o});var u=t(28481),r=t(93264);const o=function(e,n){var t=(0,r.useState)(e),o=(0,u.Z)(t,2),s=o[0],c=o[1];return(0,r.useEffect)((function(){var t=setTimeout((function(){c(e)}),n);return function(){clearTimeout(t)}}),[n,e]),s}}}]);
-//# sourceMappingURL=../sourcemaps/609.bb8c26cce6e2b0833d76.js.map
+//# sourceMappingURL=../sourcemaps/609.1bcad8cb0f07a023e9d4.js.map
--- js/6891.da42c86ee1a92ed32b28.js
+++ js/6891.da42c86ee1a92ed32b28.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/6891.bb8c26cce6e2b0833d76.js.map
+//# sourceMappingURL=../sourcemaps/6891.1bcad8cb0f07a023e9d4.js.map
