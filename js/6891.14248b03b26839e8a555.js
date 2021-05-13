f8eb17dd7b6bd229c1bd00f66f2004400cdaa7fd
Travis CI User <travis@example.org>
2021-05-13 11:51:27 +0000

    Reconfigures report rendering a wee bit for better readability (#822)
    fixes https://issues.redhat.com/browse/ADVISOR-831

--- js/6891.14248b03b26839e8a555.js
+++ js/6891.14248b03b26839e8a555.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/6891.dcb5d10b0fdd7a367131.js.map
+//# sourceMappingURL=../sourcemaps/6891.b275cc60a41889ece163.js.map
