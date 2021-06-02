69131014e6723f58df22e8b48637cbc4a10bf3ef
Travis CI User <travis@example.org>
2021-06-02 15:07:09 +0000

    Fix sentence case, also npm audit (#826)
    fixes https://issues.redhat.com/browse/RHCLOUD-12763

--- js/341.e398f6fff470fa7bc762.js
+++ js/341.e398f6fff470fa7bc762.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[341],{76341:(e,n,r)=>{"use strict";r.d(n,{Z:()=>o});var a=r(2095),t=r(85962),s=r(21015),l=r(62965),c=r(93264),d=r.n(c),m=r(45697),u=function(e){var n=e.message;return d().createElement(d().Fragment,null,d().createElement(a.Card,{className:"ins-empty-rule-cards"},d().createElement(s.CardHeader,null,d().createElement(l.ZP,{size:"lg"})),d().createElement(t.CardBody,null,n)))};const o=u;u.propTypes={message:r.n(m)().string}}}]);
-//# sourceMappingURL=../sourcemaps/341.72fffb48306cee22356b.js.map
+//# sourceMappingURL=../sourcemaps/341.04e2eea6cfb26d520631.js.map
