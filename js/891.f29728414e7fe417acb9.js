86dbe7f904232d085f60d2c1e35eed4857036d73
Travis CI User <travis@example.org>
2021-06-18 22:51:18 +0000

    Updates report detail table to use pf select/disabled select (#836)
    fixes https://issues.redhat.com/browse/RHCLOUD-14885

--- js/891.f29728414e7fe417acb9.js
+++ js/891.f29728414e7fe417acb9.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/891.b46e912fdbd5860234b7.js.map
+//# sourceMappingURL=../sourcemaps/891.e20d40461acdd3c07bbf.js.map
