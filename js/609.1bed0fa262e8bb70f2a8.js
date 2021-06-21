982fd4dbc01c2fd6716b8b53442303d9998a7059
Travis CI User <travis@example.org>
2021-06-21 21:25:27 +0000

    Inventory table to be compact variant by default

--- js/609.1bed0fa262e8bb70f2a8.js
+++ js/609.1bed0fa262e8bb70f2a8.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[609],{40609:(e,n,t)=>{"use strict";t.d(n,{Z:()=>o});var u=t(28481),r=t(93264);const o=function(e,n){var t=(0,r.useState)(e),o=(0,u.Z)(t,2),s=o[0],c=o[1];return(0,r.useEffect)((function(){var t=setTimeout((function(){c(e)}),n);return function(){clearTimeout(t)}}),[n,e]),s}}}]);
-//# sourceMappingURL=../sourcemaps/609.67c94dbfc0cec449311a.js.map
+//# sourceMappingURL=../sourcemaps/609.8d55466c37c19491f9c0.js.map
