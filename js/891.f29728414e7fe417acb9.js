a65c70719c48cbaa391746c6bf241305bec2a62f
Travis CI User <travis@example.org>
2021-06-25 15:57:18 +0000

    Update deps, add beta start, relocate rec name badge 🪃🇹🇹🪂 (#842)
    fixes https://issues.redhat.com/browse/ADVISOR-1815
    Hold off on redhat-cloud-services/frontend-components-config newer versions break proxy it appears

--- js/891.f29728414e7fe417acb9.js
+++ js/891.f29728414e7fe417acb9.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/891.297682caf1bc61648d4d.js.map
+//# sourceMappingURL=../sourcemaps/891.f22cb750a8264769c7db.js.map
