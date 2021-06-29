2184c39b118568375e3155e643d0460041e8be22
Travis CI User <travis@example.org>
2021-06-29 10:36:21 +0000

    Restoring system table pagination while using getEntities (#843)

--- js/609.1bed0fa262e8bb70f2a8.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[609],{40609:(e,n,t)=>{"use strict";t.d(n,{Z:()=>o});var u=t(28481),r=t(93264);const o=function(e,n){var t=(0,r.useState)(e),o=(0,u.Z)(t,2),s=o[0],c=o[1];return(0,r.useEffect)((function(){var t=setTimeout((function(){c(e)}),n);return function(){clearTimeout(t)}}),[n,e]),s}}}]);
-//# sourceMappingURL=../sourcemaps/609.f22cb750a8264769c7db.js.map
