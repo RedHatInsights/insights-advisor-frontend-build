59f79d0b2d32a07b0f02ccec903103370570955a
Travis CI User <travis@example.org>
2021-08-16 13:34:27 +0000

    Adds intl wrapper to RecDetails fed mod

--- js/891.f29728414e7fe417acb9.js
+++ js/891.f29728414e7fe417acb9.js
@@ -1,2 +1,2 @@
 (self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[891],{46891:(e,n,t)=>{"use strict";t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/891.343d0b7e3513e91d7a1e.js.map
+//# sourceMappingURL=../sourcemaps/891.d03e25ad7a1a15b48d08.js.map
