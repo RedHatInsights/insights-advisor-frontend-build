1a29a529682dd70cf8c9d2e146540163c82630d7
Travis CI User <travis@example.org>
2021-08-20 12:46:23 +0000

    Allows for xml to be rendered in report details (#863)
    fixes https://issues.redhat.com/browse/ADVISOR-1936

--- js/341.e398f6fff470fa7bc762.js
+++ js/341.e398f6fff470fa7bc762.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[341],{76341:(e,n,r)=>{r.d(n,{Z:()=>o});var a=r(2095),t=r(85962),s=r(21015),l=r(62965),c=r(93264),d=r.n(c),m=r(45697),u=function(e){var n=e.message;return d().createElement(d().Fragment,null,d().createElement(a.Card,{className:"ins-empty-rule-cards"},d().createElement(s.CardHeader,null,d().createElement(l.ZP,{size:"lg"})),d().createElement(t.CardBody,null,n)))};const o=u;u.propTypes={message:r.n(m)().string}}}]);
-//# sourceMappingURL=../sourcemaps/341.1b0f74931d0e79eee0e4.js.map
+//# sourceMappingURL=../sourcemaps/341.0bac546925f707307399.js.map
