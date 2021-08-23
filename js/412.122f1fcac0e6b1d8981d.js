951154fc17f8ee1925ea4e96fff14800222aa119
Travis CI User <travis@example.org>
2021-08-23 19:55:48 +0000

    Make riskOfChange optional: rule details component (#864)
    Right now, there is no resolution (risk of change) value
    available in the contents of OCP Insights rules. As RuleDetails is a
    shared component, we need to make sure the component does not crash or
    show incorrect info when this value is absent.

--- js/412.122f1fcac0e6b1d8981d.js
+++ js/412.122f1fcac0e6b1d8981d.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[412],{28412:(e,n,r)=>{r.r(n),r.d(n,{default:()=>f});var t=r(87999),a=r(55323),o=r(71355),l=r(88931),s=r(93264),c=r.n(s),u=r(16530),i=r(35240),d=r(61380),m=r(83130);const f=function(){return c().createElement(a.Pj,{locale:navigator.language.slice(0,2),messages:m,onError:console.log},c().createElement(l.Provider,{store:(0,d.S1)().getStore()},c().createElement(u.BrowserRouter,{basename:(0,i.eb)(window.location.pathname)},c().createElement(o.qJ,null),c().createElement(t.Z,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/412.3d5404d75e9ac9990f47.js.map
+//# sourceMappingURL=../sourcemaps/412.159ad277dc7154be7901.js.map
