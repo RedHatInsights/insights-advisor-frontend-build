61b5d5158e090f43e32f5cfaf3ebacdfbf3bc9ab
Travis CI User <travis@example.org>
2021-08-27 19:08:46 +0000

    Continuation of #856, transitions advisor to rtkquery (#860)
    fixes https://issues.redhat.com/browse/ADVISOR-1879

--- js/891.f29728414e7fe417acb9.js
+++ js/891.f29728414e7fe417acb9.js
@@ -1,2 +1,2 @@
 "use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[891],{46891:(e,n,t)=>{t.d(n,{Z:()=>d});var r=t(2095),l=t(85962),a=t(24561),u=t(93264),c=t.n(u);const d=function(){return c().createElement(c().Fragment,null,c().createElement(r.Card,null,c().createElement(l.CardBody,null,c().createElement(a.aV,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/891.5bdde3967002bc2c3a2e.js.map
+//# sourceMappingURL=../sourcemaps/891.baa6b9c739a0ec6b6f2d.js.map
