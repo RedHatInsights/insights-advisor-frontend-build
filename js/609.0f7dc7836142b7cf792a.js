36f73a2764be58291a87639c8768e738d1d53980
Travis CI User <travis@example.org>
2021-08-31 19:23:36 +0000

    Adds pathways table component (#868)
    fixes https://issues.redhat.com/browse/ADVISOR-1883

---
+++ js/609.0f7dc7836142b7cf792a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[609],{40609:(e,n,t)=>{t.d(n,{Z:()=>o});var u=t(29439),r=t(93264);const o=function(e,n){var t=(0,r.useState)(e),o=(0,u.Z)(t,2),s=o[0],c=o[1];return(0,r.useEffect)((function(){var t=setTimeout((function(){c(e)}),n);return function(){clearTimeout(t)}}),[n,e]),s}}}]);
+//# sourceMappingURL=../sourcemaps/609.ceaee3f7851307d9e635ac8ad01c6401.js.map
