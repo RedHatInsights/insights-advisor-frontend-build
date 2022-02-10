08ee43c61e3aaa603d473712b3202ea640c26b17
Travis CI User <travis@example.org>
2022-02-10 17:11:38 +0000

    Advisor-2182: disable autocollapse first row (#896)

--- js/6105.1644512154988.d11a464cd590b78ab1a6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6105],{33033:(e,r,a)=>{a.r(r),a.d(r,{default:()=>d.Z});var d=a(42544)},99805:(e,r,a)=>{a.r(r),a.d(r,{default:()=>d.Z});var d=a(31566)}}]);
