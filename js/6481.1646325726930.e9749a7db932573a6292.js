7445120147991efdffa5187fa47e7069a8f1410c
Travis CI User <travis@example.org>
2022-03-07 23:01:45 +0000

    fix: SystemAdvisor table row count is fixed (#917)

--- js/6481.1646325726930.e9749a7db932573a6292.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.0eeedc6d928b29a0c43c7fe7b6cc26ee.js.map
