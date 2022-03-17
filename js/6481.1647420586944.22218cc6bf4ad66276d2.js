d5b853529513f2fe47ec7ede3a4e65321e5ccd63
Travis CI User <travis@example.org>
2022-03-17 10:49:01 +0000

    chore: add ouiaId to tour button (#923)

--- js/6481.1647420586944.22218cc6bf4ad66276d2.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.ed62ee7b1f34ff50248f06ce00c30558.js.map
