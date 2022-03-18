39de25e57d5a879788c415c04beea1281350c0dd
Travis CI User <travis@example.org>
2022-03-18 11:21:36 +0000

    fix(RuleDetails): ADVISOR-2054 - Recommendation description padding (#922)

---
+++ js/6481.1647602345087.60fcd3375ab7d4053400.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.409e7bdbcdc2b3c1240b59fdc7c66fb2.js.map
