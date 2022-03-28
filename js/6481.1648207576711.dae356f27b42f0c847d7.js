2b0e2e4fde4b62cc4482eceb050bcb4cc54327f0
Travis CI User <travis@example.org>
2022-03-28 13:48:56 +0000

    fix(SystemPdf): ADVISOR-2367 - add safe navigation (#934)

--- js/6481.1648207576711.dae356f27b42f0c847d7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.ff8f666f2a1423dee60a127a6d511ea5.js.map
