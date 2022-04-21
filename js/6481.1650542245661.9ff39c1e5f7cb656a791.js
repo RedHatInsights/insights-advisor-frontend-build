efd71d24f6b1aea2188c8f931bcf8fcad88e092a
Travis CI User <travis@example.org>
2022-04-21 12:44:21 +0000

    fix(Pathways): ADVISOR-2119 fix missing category name when only one (#958)
    Co-authored-by: Samuel Olekšák <soleksak@redhat.com>

--- js/6481.1650542245661.9ff39c1e5f7cb656a791.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.ef6e05f142ca3366496b77bfd5bb88fe.js.map
