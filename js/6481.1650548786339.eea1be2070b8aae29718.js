04200e5df8e4572b4b03c1228245831bc1a2fda9
Travis CI User <travis@example.org>
2022-04-21 13:49:16 +0000

    ADVISOR-2384 (#944)
    When pressing the 'Reset Filter' button on a pathways details page, the filter would reset the pathway to null, and then it would just show all recommendations regardless if they were related to the pathway.
    
    To see changes,
    Go to advisor, navigate to pathways table , and select a pathway. The default filters are currently 'systems impacted' and ' status enabled'. Before the change, if you pressed reset filter, it would show all recommendations, now it should still show recommendations for specific pathway.
    This change also removes the 'pathway' param in the URL as to not
    duplicate it
    
    Co-authored-by: Adonis <adonis@localhost-live.attlocal.net>

---
+++ js/6481.1650548786339.eea1be2070b8aae29718.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.3003182d1567285d844d2b02d4ea2d04.js.map
