128d7a1cb84d7dc87e683265e8cc242f13961f5d
Travis CI User <travis@example.org>
2022-04-21 12:58:39 +0000

    fix(Pathways): ADVISOR-2118 fix card and chart size (#956)
    Co-authored-by: Samuel Olekšák <soleksak@redhat.com>

---
+++ js/6481.1650545752468.2cc17301fe80e50f7cfe.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.5b6bd31a5d53da47fe4e22e42b0fba6c.js.map
