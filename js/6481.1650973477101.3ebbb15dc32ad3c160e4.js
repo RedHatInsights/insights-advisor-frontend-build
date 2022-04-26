9f164017774a566fc896f2194068a736f2787a9a
Travis CI User <travis@example.org>
2022-04-26 11:47:33 +0000

    Open feedback modal on rule rating (#954)

---
+++ js/6481.1650973477101.3ebbb15dc32ad3c160e4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.cf29967650b8a2a01cdd6c4b21203c52.js.map
