8433b84ce0c1105f164360db3873b6f5ad1cfaaf
Travis CI User <travis@example.org>
2022-04-29 19:11:24 +0000

    fix image name and component (#969)

---
+++ js/6481.1651259300378.560c506de59032056ec7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.b0195246c54ed8bd39e1a60248f9d868.js.map
