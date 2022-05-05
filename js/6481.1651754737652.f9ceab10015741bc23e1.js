99638cd5ca1dad783729fcbf50c22c0ad80fd8f1
Travis CI User <travis@example.org>
2022-05-05 13:39:22 +0000

    ADVISOR-2462, bulk selector dropdown is improved.

--- js/6481.1651754737652.f9ceab10015741bc23e1.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.29977559bf9c8cb7ba07691c540741bd.js.map
