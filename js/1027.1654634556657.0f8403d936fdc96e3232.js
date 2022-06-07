08ec6ad7ddd9b2bf10f92156bf3c541a5b1ff2c3
Travis CI User <travis@example.org>
2022-06-07 20:45:30 +0000

    bulkSelect in Systems is parralel to recommendations

---
+++ js/1027.1654634556657.0f8403d936fdc96e3232.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[1027],{91027:()=>{}}]);
---
+++ js/4113.1654634556657.0f8403d936fdc96e3232.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[4113],{14113:()=>{}}]);
