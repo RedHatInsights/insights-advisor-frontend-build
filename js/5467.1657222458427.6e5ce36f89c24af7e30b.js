875c4d69e38af1d6b9b737ec31acdef0d574f050
Travis CI User <travis@example.org>
2022-07-08 16:31:53 +0000

    Refresh Bug

--- js/5467.1657222458427.6e5ce36f89c24af7e30b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[5467,9477,3929],{45467:()=>{}}]);
--- js/7350.1657222458427.6e5ce36f89c24af7e30b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[7350],{77350:()=>{}}]);
