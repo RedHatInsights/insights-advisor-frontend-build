86b27610ee053696dff373721c7b3fc2b46c604e
Travis CI User <travis@example.org>
2022-07-11 08:02:10 +0000

    refactor(System details): Import ReportDetails from advisor-components (#997)
    * Remove custom ReportDetails component
    
    * Do not expose the ReportDetails federated module
    
    * Import ReportDetails from advisor-components

--- js/311.1657297738633.3f35817ff0cc9aa46165.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[311],{50311:()=>{}}]);
--- js/6481.1657297738633.3f35817ff0cc9aa46165.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
-//# sourceMappingURL=../sourcemaps/6481.9f3a7c9f23a42374bd2b88a95e6eecce.js.map
