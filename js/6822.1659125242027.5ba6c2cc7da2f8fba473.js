e9773b8b4a9f6d135965ae80e1ba5785d110250f
Travis CI User <travis@example.org>
2022-07-29 20:10:26 +0000

    feat(ADVISOR-2122) - Reorg of pathways card (#1004)

---
+++ js/6822.1659125242027.5ba6c2cc7da2f8fba473.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6822],{66822:()=>{}}]);
---
+++ js/8379.1659125242027.5ba6c2cc7da2f8fba473.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[8379],{98379:()=>{}}]);
