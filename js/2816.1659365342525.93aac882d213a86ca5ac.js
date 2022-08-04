e871c8943460e415774b88a32b482d1cba2aa6cc
Travis CI User <travis@example.org>
2022-08-04 19:29:04 +0000

    chore(labels) update stragling labels to be compact' (#1014)

--- js/2816.1659365342525.93aac882d213a86ca5ac.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[2816],{72816:()=>{}}]);
--- js/3519.1659365342525.93aac882d213a86ca5ac.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[3519],{53519:()=>{}}]);
