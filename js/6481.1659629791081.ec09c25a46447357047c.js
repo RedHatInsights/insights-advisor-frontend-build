255d8ce68596a879510756c247b6ed7073495c61
Travis CI User <travis@example.org>
2022-08-04 16:19:28 +0000

    chore(labels) update stragling labels to be compact' (#1014)

---
+++ js/6481.1659629791081.ec09c25a46447357047c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.3139c5c90bd14b0f42a1e07ed15e3c90.js.map
