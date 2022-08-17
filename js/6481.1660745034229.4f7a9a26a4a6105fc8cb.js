a74b4d4b3c1491187fd86822f67cee2e846dd5c5
Travis CI User <travis@example.org>
2022-08-17 14:06:57 +0000

    Revert "feat(ADVISOR-2546) : Expanded first Item on page load (#1000)"
    This reverts commit 40883bf1d111b5239455d23e5cab34f4b1122295.

---
+++ js/6481.1660745034229.4f7a9a26a4a6105fc8cb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.199f10516d729083644366a96df3f452.js.map
