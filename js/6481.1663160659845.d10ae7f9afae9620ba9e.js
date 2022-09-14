57b69873d4840e3060d5a7adbb7915dbb501bb9a
Travis CI User <travis@example.org>
2022-09-14 13:07:15 +0000

    Redo README.md to be up to date (#1025)
    * Redo README.md to be up to date
    
    * Add notice about stable envs

---
+++ js/6481.1663160659845.d10ae7f9afae9620ba9e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.eeb8df1c132c0d5ef2329e9c98084878.js.map
