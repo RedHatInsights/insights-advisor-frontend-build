79478f8a74310abb86224f9f641f071d22e29102
Travis CI User <travis@example.org>
2022-09-30 19:40:08 +0000

    Feat(Recs system table): Add impacted date column
    * Implements https://issues.redhat.com/browse/ADVISOR-2191

---
+++ js/6481.1664566624069.ef500206c2843a36c4f5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.01e829d4099004da75bdcbba9ac9fc7a.js.map
---
+++ js/8024.1664566624069.ef500206c2843a36c4f5.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[8024],{38024:()=>{}}]);
