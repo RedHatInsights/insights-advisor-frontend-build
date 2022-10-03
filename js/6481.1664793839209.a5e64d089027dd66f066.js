3c967e568fc126ba07e7cf34a3147d2f10cb19ea
Travis CI User <travis@example.org>
2022-10-03 10:46:16 +0000

    fix(ADVISOR-2465): resolve broken wording in empty states

---
+++ js/6481.1664793839209.a5e64d089027dd66f066.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6481],{46481:(t,a,e)=>{e.d(a,{l:()=>o,Z:()=>n});const o={name:"--pf-c-tooltip--MaxWidth",value:"18.75rem",var:"var(--pf-c-tooltip--MaxWidth)"},n=o}}]);
+//# sourceMappingURL=../sourcemaps/6481.4cb73a2cc85a38428d0e3c48c9b615cd.js.map
