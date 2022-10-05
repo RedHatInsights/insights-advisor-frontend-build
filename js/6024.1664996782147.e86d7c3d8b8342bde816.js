02e971936ee7b2641fbdfb679797c29b7690efb0
Travis CI User <travis@example.org>
2022-10-05 20:42:37 +0000

    fix(SystemTable): update the OS column sort key

--- js/6024.1664996782147.e86d7c3d8b8342bde816.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[6024],{56024:()=>{}}]);
---
+++ js/9477.1665002370089.e04bbef0285be8150660.js
@@ -0,0 +1,1 @@
+(self.webpackChunkadvisor_frontend=self.webpackChunkadvisor_frontend||[]).push([[9477,5467],{45467:()=>{}}]);
